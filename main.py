753a491e27dbb207b51246adb145d6cfb7c04ef2
Andrew Ma <ama25@belmontschools.net>
2024-07-20 17:28:31 -0400

    pet manager and file loading

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import time
 import json
 
+PET_TO_ASCII = {
+    "dog": r"""
+    / \__
+   (    @\___
+   /         O
+  /   (_____/
+ /_____/   U
+""",
+    "cat": r"""
+ /\_/\  
+( o.o ) 
+ > ^ <
+""",
+    "bird": r"""
+   __
+ <(o )___
+   (  ._>
+   """}
+
+
 class Pet:
     def __init__(self, name, age, species, health, hunger, happiness, energy, last_fed, last_played, last_slept):
         self.name = name
@@ -35,20 +55,38 @@ class Pet:
     def __str__(self):
         return f"{self.name} is a {self.species} that is {self.age} years old. It has {self.health} health, {self.hunger} hunger, {self.happiness} happiness, and {self.energy} energy."
 
+
+class PetManager:
+    def __init__(self):
+        self.pets = []
+
+    def add_pet(self, pet):
+        self.pets.append(pet)
+
     def save(self):
-        with open("pet.json", "w") as f:
-            json.dump(self.__dict__, f)
+        pets_data = [pet.__dict__ for pet in self.pets]
+        with open("pets.json", "w") as f:
+            json.dump(pets_data, f)
 
     def load(self):
-        with open("pet.json", "r") as f:
-            data = json.load(f)
-            self.name = data["name"]
-            self.age = data["age"]
-            self.species = data["species"]
-            self.health = data["health"]
-            self.hunger = data["hunger"]
-            self.happiness = data["happiness"]
-            self.energy = data["energy"]
-            self.last_fed = data["last_fed"]
-            self.last_played = data["last_played"]
-            self.last_slept = data["last_slept"]
+        with open("pets.json", "r") as f:
+            pets_data = json.load(f)
+            self.pets = [Pet(**data) for data in pets_data]
+
+
+if __name__ == "__main__":
+    manager = PetManager()
+    dog = Pet("Pedro", 5, "dog", 100, 100, 100, 100, time.time(), time.time(), time.time())
+    cat = Pet("Whiskers", 3, "cat", 100, 100, 100, 100, time.time(), time.time(), time.time())
+    manager.add_pet(dog)
+    manager.add_pet(cat)
+    manager.save()
+    manager.load()
+    for pet in manager.pets:
+        pet.feed()
+        pet.play()
+        pet.sleep()
+        pet.update()
+        print(pet)
+        print(PET_TO_ASCII[pet.species])
+
